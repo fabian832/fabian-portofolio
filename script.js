75e068c5af252c85add62702c075fcaed6e9b7ff
fabian832 <fabianhie@gmail.com>
2024-05-29 12:25:31 +0700

    [contact-me] fix maillllllllllllllll

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ function sendmail() {
     emailjs.send("service_0xgk60o", "template_8y5upv8", params)
     emailjs.send("service_0xgk60o", "template_zqfmypm", params).then(function(response) {
         alert("Email sent!");
-        window.location = "/";
     }, function(error) {
         alert("Failed to send email. Error: " + JSON.stringify(error));
     });
-    window.location= "index.html"
+}
+
+function resetmail(){
+    document.getElementById("email").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
 }
